2420765aca6eb464158b951bc16e95a0c86126ea
Gregory Cavelier <gregory.cavelier@cnieg.fr>
2025-12-29 16:57:18 +0100

    visual bug in branch main

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,91 @@
-use ratatui::buffer::Buffer;
-use ratatui::style::{Color, Stylize};
-use ratatui::text::Line;
+use ratatui::{
+    Frame,
+    crossterm::{
+        self,
+        event::{self, KeyCode},
+    },
+    layout::{Alignment, Constraint, Layout, Rect},
+    style::{Color, Style, Stylize},
+    widgets::{Block, BorderType::Rounded, Borders, Clear, Padding, Paragraph, Wrap},
+};
 
-fn main() {
-    let prev_no_color = Buffer::with_lines([Line::from("   ")]);
-    let prev_with_color = Buffer::with_lines([Line::from("   ").fg(Color::Red)]);
-    let next = Buffer::with_lines([Line::from("⚠️")]);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut exit = false;
+    let mut popup = false;
 
-    let diff = prev_no_color.diff(&next);
+    ratatui::run(|terminal| {
+        while !exit {
+            terminal.draw(|frame| render(frame, popup))?;
 
-    println!("diff with prev_no_color:");
-    for (x, y, cell) in &diff {
-        println!("({}, {}): {:?}", x, y, cell);
-    }
+            // Wait for an event
+            let event = crossterm::event::read().unwrap();
 
-    assert!(
-        !diff.iter().any(|(x, y, _)| *x == 1 && *y == 0),
-        "Diff should not include trailing cell (1,0) when only style changed. \
-            Found updates: {:?}",
-        diff.iter()
-            .map(|(x, y, c)| (x, y, c.symbol(), c.fg))
-            .collect::<Vec<_>>()
+            match event {
+                crossterm::event::Event::Key(key_event) => {
+                    if key_event.kind == event::KeyEventKind::Press {
+                        match key_event.code {
+                            KeyCode::Char('q') | KeyCode::Esc => exit = true,
+                            KeyCode::Char('d') => popup = true,
+                            _ => (),
+                        }
+                    }
+                }
+                _ => (),
+            }
+        }
+        Ok(())
+    })
+}
+
+#[rustfmt::skip]
+pub fn render(frame: &mut Frame, popup: bool) {
+    frame.render_widget(
+        Paragraph::new("TEST\n123456789")
+            .block(Block::default().borders(Borders::ALL).border_type(Rounded))
+            .fg(Color::LightBlue) // If this line is commented, render_popup() is ok!
+        ,frame.area(),
     );
 
-    let diff = prev_with_color.diff(&next);
-
-    println!("\n\ndiff with prev_with_color:");
-    for (x, y, cell) in &diff {
-        println!("({}, {}): {:?}", x, y, cell);
+    if popup {
+        render_popup(
+            frame,
+            "⚠️ Test ⚠️",
+            Paragraph::new(format!("message 0123456798 message")),
+        );
     }
+}
 
-    assert!(
-        !diff.iter().any(|(x, y, _)| *x == 1 && *y == 0),
-        "Diff should not include trailing cell (1,0) when only style changed. \
-            Found updates: {:?}",
-        diff.iter()
-            .map(|(x, y, c)| (x, y, c.symbol(), c.fg))
-            .collect::<Vec<_>>()
+fn render_popup(frame: &mut Frame, title: &str, msg: Paragraph) {
+    let area = Rect::new(3, 3, 30, 5);
+
+    let top_block = Block::default()
+        .title(title)
+        .title_style(Style::new().gray())
+        .title_alignment(Alignment::Center)
+        .borders(Borders::TOP | Borders::LEFT | Borders::RIGHT)
+        .border_style(Color::LightBlue)
+        .border_type(Rounded);
+
+    frame.render_widget(Clear, area);
+
+    let [top_chunk, bottom_chunk] =
+        Layout::vertical([Constraint::Fill(1), Constraint::Length(4)]).areas(area);
+
+    frame.render_widget(
+        msg.wrap(Wrap { trim: false })
+            .block(top_block.padding(Padding {
+                left: 1,
+                right: 1,
+                top: 1,
+                bottom: 1,
+            })),
+        top_chunk,
     );
+
+    // Render bottom border
+    let bottom_block = Block::default()
+        .borders(Borders::BOTTOM | Borders::LEFT | Borders::RIGHT)
+        .border_style(Color::LightBlue)
+        .border_type(Rounded);
+    frame.render_widget(bottom_block, bottom_chunk);
 }
